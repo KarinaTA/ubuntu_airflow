9ef6f8371015d6c97c4003b583581f4f474fe9ea
KarinaTA <karishka2209@gmail.com>
2025-08-30 18:08:39 +0500

    Modified my_first_dug v2

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -11,7 +11,7 @@ default_args={
 }
 
 with DAG(
-    dag_id='my_first_dag',
+    dag_id='my_first_dag_v2',
     description='This is my first dag that I wrote',
     default_args=default_args,
     start_date=datetime(2025, 8, 28, 17),
@@ -20,9 +20,22 @@ with DAG(
     catchup=False
     
 ) as dag:
+    
     task_1 = BashOperator(
         task_id='task_1',
         bash_command='echo "Hello, world! This is the first task!"'
     )
     
-    task_1
+    task_2 = BashOperator(
+        task_id='task_2',
+        bash_command='echo "This is the second task and will be running after the first task!"'
+    )
+    
+    task_3 = BashOperator(
+        task_id='task_3',
+        bash_command='echo "This is the third task and will be running at the same time as the second task!"'
+    )
+    
+    # Task dependancy method 1 
+    task_1 >> task_2
+    task_1 >> task_3
